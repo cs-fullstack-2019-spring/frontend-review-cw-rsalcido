49f7d25e195bef8b14ffb294769533b26c2c6780
Ricardo Salcido <rsalcido@liberty.edu>
2019-02-05 16:40:56 -0600

    GWW5T

---
+++ Week5GWT.js
@@ -0,0 +1,13 @@
+// Create a button with "Click Me".
+// Once it's clicked, ask the user if they want to continue being asked to quit.' +
+// 'If they enter 'n', stop asking. If they enter anything else, ask again.
+
+var btn = document.getElementById("BUTTON");
+
+
+
+let button;
+let userinput;
+    while (userinput != 'n')
+        userinput = prompt("Would you like to quit?");
+
